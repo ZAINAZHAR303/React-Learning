4bd3ccd92e1390e8c4b0e61ebac51a908e4513a4
zain <zainazhar303@gmail.com>
2024-10-10 01:24:36 +0500

    Render and UseState

---
+++ react-render/src/components/UseState.jsx
@@ -0,0 +1,15 @@
+import React, { useCallback, useState } from 'react'
+
+const UseState = () => {
+    const [count,setCount] = useState(0)
+    console.log("state render")
+  return (
+    <div>
+      <button className='flex '  onClick={()=>setCount(prev=>prev+1)}>count _ {count}</button>
+      <button onClick={()=>setCount(0)}>count to 0</button>
+      <button onClick={()=>setCount(5)}>count to 5</button>
+    </div>
+  )
+}
+
+export default UseState

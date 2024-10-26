98e64332931eccf80abb0b046ded230931b66a34
zain <zainazhar303@gmail.com>
2024-10-26 21:41:00 +0500

    template and not-found file in nextjs

--- next_1/src/app/(auth)/template.jsx
+++ next_1/src/app/(auth)/template.jsx
@@ -1,16 +1,18 @@
 "use client";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-
+import { useState } from "react";
 const navlinks = [
   { name: "Register", href: "/register" },
   { name: "Login", href: "/login" },
   { name: "Forgot Password", href: "/forgotpassword" },
 ];
 export default function layout({ children }) {
+  const [input, setInput] = useState("")
   const pathname = usePathname();
   return (
     <div>
+      <input type="text" placeholder="search" value={input} onChange={(e) => setInput(e.target.value)}   />
       {navlinks.map((link) => {
         const isactive = pathname.startsWith(link.href);
         return (
---
+++ next_1/src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='h-screen w-full flex items-center justify-center bg-green-200'>
+      <h1 className='text-4xl font-bold text-gray-400'>This page is not available</h1>
+    </div>
+  )
+}
+
+export default NotFound

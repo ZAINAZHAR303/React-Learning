3bdbf2616bad33e51869f3489b629024d50bdc5b
zain <zainazhar303@gmail.com>
2024-11-06 15:30:55 +0500

    uses middleware in redux

--- react-redux/index.js
+++ react-redux/index.js
@@ -1,9 +1,13 @@
 // console.log("from index.js")
 const redux = require("redux");
+const reduxLogger = require("redux-logger");
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 const BUY_CAKE = "BUY_CAKE";
-const BUY_ICECREAM = "BUY_ICECREAM"; 
+const BUY_ICECREAM = "BUY_ICECREAM";
 function buycake() {
   return {
     type: BUY_CAKE,
@@ -11,17 +15,16 @@ function buycake() {
   };
 }
 function buyIceCream() {
-    return {
-      type: BUY_ICECREAM,
-      
-    };
-  }
-const initialCakeState = {
-    numOfCakes: 10,
+  return {
+    type: BUY_ICECREAM,
   };
-  const initialIceCreamState = {
-    numOfIceCreams: 20,
-  }
+}
+const initialCakeState = {
+  numOfCakes: 10,
+};
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
 const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
@@ -35,27 +38,27 @@ const cakeReducer = (state = initialCakeState, action) => {
   }
 };
 const iceCreamReducer = (state = initialIceCreamState, action) => {
-    switch (action.type) {
-      case BUY_ICECREAM:
-        return {
-          ...state,
-          numOfIceCreams: state.numOfIceCreams - 1,
-        };
-      default: {
-        return state;
-      }
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    default: {
+      return state;
     }
-  };
+  }
+};
 
-  const rootReducer = combineReducers({
-    cake: cakeReducer,
-    iceCream: iceCreamReducer,
-  })
-const store = createStore(rootReducer);
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
 console.log("initial state", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log("updated state", store.getState())
-);
+const unsubscribe = store.subscribe(() => {});
 
 store.dispatch(buycake());
 store.dispatch(buycake());

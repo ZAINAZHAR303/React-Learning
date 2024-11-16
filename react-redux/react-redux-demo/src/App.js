2b3ab1f3f721e6f510e41d424d9fd09b2f8c2933
zain <zainazhar303@gmail.com>
2024-11-17 00:47:58 +0500

    implement store to components

--- react-redux/react-redux-demo/src/App.js
+++ react-redux/react-redux-demo/src/App.js
@@ -1,12 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Provider } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import store from "./components/redux/store";
+import { fetchProducts } from "./store/slices/productSlice";
 function App() {
+  const products = useSelector(store => store.productSlice.products)
+  const dispatch = useDispatch();
+  console.log("products in comp", products)
+
+  const onClickGetProducts = ()=>{
+    dispatch(fetchProducts())
+  }
   return (
-    <Provider store={store}>
-      <div className="App"></div>
-    </Provider>
+    
+      <div className="App">
+        <button onClick={onClickGetProducts}>GEt products</button>
+      </div>
+  
   );
 }
 

1dc995eb9fe123a4acf36db394a82d47985b2704
zain <zainazhar303@gmail.com>
2024-11-25 00:11:16 +0500

    change Routing.jsx for testing

--- react-redux/react-redux-demo/src/routing/Routing.jsx
+++ react-redux/react-redux-demo/src/routing/Routing.jsx
@@ -1,8 +1,19 @@
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <div>sff</div>
-    }
-])
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <div>sff</div>
+
+//     }
+// ])
+import React from 'react'
+
+export default function Routing() {
+  return (
+    <div>
+      <h1>checking TestBranch</h1>
+    </div>
+  )
+}
+
